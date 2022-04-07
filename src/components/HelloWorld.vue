92cfd6c45e0eb9155afe0009a5fffba67eab99e0
Megan Honig <megan@voicethread.net>
2022-04-07 15:30:54 -0400

    chore: back to importing from 'swiper/vue'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,28 +2,22 @@
 import { ref } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 
-import { A11y } from 'swiper'
-
 defineProps({
   msg: String
 })
 
 const slides = ref(Array(3))
 
-const a11y = ref(A11y)
-
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <!-- <Swiper>
+  <Swiper>
     <template v-for="(_, index) in slides">
       <SwiperSlide>{{ index + 1 }}</SwiperSlide>
     </template>
-  </Swiper> -->
-
-  {{ a11y }}
+  </Swiper>
 
 </template>
 
