4fc717cc17b8ca6f79d86c39f2d34cd1db745063
Megan Honig <megan@voicethread.net>
2022-04-07 15:44:08 -0400

    chore: use jest-transform-stub to treat esm as a non-js asset

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
   testMatch: ['**/(*.)unit.js'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    // '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-    //   'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.js?$': 'babel-jest'
   },
   moduleNameMapper: {
-    'swiper': '<rootDir>/node_modules/swiper/swiper-bundle.js'
+    'swiper': 'jest-transform-stub'
   }
 }
